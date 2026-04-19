4222c09daec46d423c6123bfbccc4e73f66ea37d
mari01ss <mariyam.sabyrkul@gmail.com>
2026-04-19 21:22:34 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 system_prompt = """
 You are SmartCar AI assistant.
